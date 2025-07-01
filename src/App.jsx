df857072a41b5b4a84078e65e51a8a58acd1d33f
Ashim Maharjan <ciferulquiorra008@gmail.com>
2025-07-01 13:13:29 +1000

    tailwind classes config setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import gsap from "gsap/all";
+import gsap from "gsap";
 
 import { ScrollTrigger, SplitText } from "gsap/all";
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="flex-center h-[100vh]">
+      <h1 className="text-3xl text-indigo-400">Hello Gsap</h1>
+    </div>
+  );
 };
 
 export default App;
